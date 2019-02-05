dba7453d23be8a292e2e05df4c54ad3f22cda2d1
Evgeny <jeka_icheev@mail.ru>
2019-02-05 15:57:38 +0500

    corrections

--- homework/Cook_book.py
+++ homework/Cook_book.py
@@ -1,14 +1,8 @@
 
-with open('Cook_book.txt') as f:
+from pprint import pprint
 
-    import pprint
-
-    # def dictionary():
-    #     cook_book = {}
-    # dictionary()
-
-    # cook_book = {}
-    def get_cook_book():
+def get_cook_book():
+    with open('Cook_book.txt') as f:
         cook_book = {}
         for line in f:
             keys = line
@@ -26,33 +20,69 @@ with open('Cook_book.txt') as f:
                 dishes['measure'] = value[2]
                 cook_book[keys.strip()].append(dishes)
             empty_line = f.readline()
-        # pprint.pprint(cook_book)
+        # pprint(cook_book)
         return cook_book
 
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    cook_book = get_cook_book()
+    # person_count = int(input('Введите количество персон:'))
+    for dish in dishes:
+        for ingredients in cook_book[dish]:
+            menu = dict(ingredients)
+            menu['quantity'] *= int(person_count)
+            if menu['ingridient_name'] not in shop_list:
+                shop_list[menu['ingridient_name']] = menu
+            else:
+                shop_list[menu['ingridient_name']]['quantity'] += menu['quantity']
+    pprint(shop_list)
+        # return shop_list
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+
+
+
+# with open('Cook_book.txt') as f:
+#
+#     from pprint import pprint
+#
+#     def get_cook_book():
+#         cook_book = {}
+#         for line in f:
+#             keys = line
+#             # print(keys.strip())
+#             number = int(f.readline())
+#             while number != 0:
+#                 value = f.readline().strip().split('|')
+#                 number -= 1
+#                 # print(value)
+#                 if keys.strip() not in cook_book.keys():
+#                     cook_book[keys.strip()] = list()
+#                 dishes = dict()
+#                 dishes['ingridient_name'] = value[0]
+#                 dishes['quantity'] = int(value[1])
+#                 dishes['measure'] = value[2]
+#                 cook_book[keys.strip()].append(dishes)
+#             empty_line = f.readline()
+#         # pprint.pprint(cook_book)
+#         return cook_book
 
     # get_cook_book()
 
-    def get_shop_list_by_dishes(dishes, person_count):
-        shop_list = {}
-        cook_book = get_cook_book()
-        # person_count = int(input('Введите количество персон:'))
-        for dish in dishes:
-            for ingredients in cook_book[dish]:
-                menu = dict(ingredients)
-                menu['quantity'] *= int(person_count)
-                if menu['ingridient_name'] not in shop_list:
-                    shop_list[menu['ingridient_name']] = menu
-                else:
-                    shop_list[menu['ingridient_name']]['quantity'] += menu['quantity']
-        print(shop_list)
-        # return shop_list
-
-
-    get_shop_list_by_dishes(['Омлет', 'Омлет'], 2)
-
-
-
-
+    # def get_shop_list_by_dishes(dishes, person_count):
+    #     shop_list = {}
+    #     cook_book = get_cook_book()
+    #     # person_count = int(input('Введите количество персон:'))
+    #     for dish in dishes:
+    #         for ingredients in cook_book[dish]:
+    #             menu = dict(ingredients)
+    #             menu['quantity'] *= int(person_count)
+    #             if menu['ingridient_name'] not in shop_list:
+    #                 shop_list[menu['ingridient_name']] = menu
+    #             else:
+    #                 shop_list[menu['ingridient_name']]['quantity'] += menu['quantity']
+    #     pprint(shop_list)
+    #     # return shop_list
+    # get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
 
 # Нужно написать функцию, которая на вход принимает список блюд из cook_book и количество персон для кого мы будем готовить
 #
@@ -71,10 +101,6 @@ with open('Cook_book.txt') as f:
 #   'Чеснок': {'measure': 'зубч', 'quantity': 6}
 # }
 # Обратите внимание, что ингредиенты могут повторятьс
-
-
-
-
 
 
 
