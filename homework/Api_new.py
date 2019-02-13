1dae790face6db18cc10f72d66972a13dc14c4b4
Evgeny <jeka_icheev@mail.ru>
2019-02-13 15:07:33 +0500

    corrections

--- homework/Api_new.py
+++ homework/Api_new.py
@@ -2,7 +2,7 @@ import requests
 from pprint import pprint
 
 API_KEY = 'trnsl.1.1.20190210T101020Z.da8831fcfb249567.b6641f9a89686edbea4ceb60c784b9b510a99fa0'
-URL = 'https://translate.yandex.net/api/v1/tr.json/translate?id=07b70d9d.5c5ff8d2.55592c23-1-0&srv=tr-text&lang=ru-en&reason=auto'
+URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 
 def translate_it(text, text_translations, lang, to_lang='ru'):
     """
@@ -17,30 +17,29 @@ def translate_it(text, text_translations, lang, to_lang='ru'):
     :return:
     """
     with open(text, encoding='utf8') as f:
-        for line in f:
-            text = line.strip()
-            empty_line = f.readline()
-    print(text)
+        text = f.read()
+    # print(text)
 
     params = {
         'key': API_KEY,
         'text': text,
         'lang': '{}-{}'.format(lang, to_lang),
     }
+    print(params['text'])
 
     response = requests.get(URL, params=params)
 
-    print(response.status_code)
+    # print(response.status_code)
     json_ = response.json()
+    new_text = ''.join(json_['text'])
 
     with open(text_translations, 'w', encoding='utf8') as f:
         print('Запись в', text_translations)
-        f.write(''.join(json_['text']))
+        f.write(new_text)
 
-    return ''.join(json_['text'])
 
 translate_it('DE.txt', 'DE2.txt', 'de')
-# translate_it('ES.txt', 'ES2.txt', 'es')
+translate_it('ES.txt', 'ES2.txt', 'es')
 translate_it('FR.txt', 'FR2.txt', 'fr')
 
 # print(translate_it('В настоящее время доступна единственная опция — признак включения в ответ автоматически определенного языка переводимого текста. Этому соответствует значение 1 этого параметра.', 'ru', 'de'))
