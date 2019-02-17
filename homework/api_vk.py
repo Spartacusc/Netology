b836c34689386ff8550de425ad149e280ba97ec5
Evgeny <jeka_icheev@mail.ru>
2019-02-17 11:23:28 +0500

    corrections

--- homework/api_vk.py
+++ homework/api_vk.py
@@ -12,42 +12,46 @@ def get_token():
         'response_type': 'token',
         'v': '5.92'
     }
-    # print('?'.join((AUTH_URL, urlencode(auth_data))))
+    print('?'.join((AUTH_URL, urlencode(auth_data))))
 
-TOKEN = '7f1964d746efb6d5934bba9bcb5f865a68bf0c0ba6f3b4999fdc77397cd5a451f4a90e52bbd63d1f767bb'
+# get_token()
+
+TOKEN = '3d0c3cfd9e22d97eb98b076968301289f4a7574269a7be622f8ae66e450881c09e5120cb8957988a42875'
 
 class User:
 
-    def __init__(self, id=None):
-        self.id = id
+    def __init__(self, user_id):
+        self.user_id = user_id
         self.token = TOKEN
 
-    # def __new__(cls,id):
-    #     ID = id
-    #     user = 'http:\VK.com\id' + str(ID)
-    #     return user
+    def __str__(self):
+        user = 'http:\VK.com\id'
+        return user + str(self.user_id)
 
     def get_params(self):
        return {
             'v': '5.92',
             'access_token': TOKEN,
-            'source_uid': 70680070,
-            'target_uid': self.id
+            'source_uid': None,
+            'target_uid': self.user_id
         }
 
     def __and__(self, other):
         params = self.get_params()
-        # print(params)
-        response = requests.get('https://api.vk.com/method/friends.getMutual', params)
-        friends_dict = response.json()
-        print('Количество общих друзей: ', len(friends_dict['response']))
-        return friends_dict
+        params['source_uid'] = self.user_id
+        params['target_uid'] = other.user_id
+        request_url = 'https://api.vk.com/method/'
+        api_method = 'friends.getMutual'
+        data = requests.get(f'{request_url}{api_method}', params).json()
+        # pprint(data)
+        print('Количество общих друзей: ', len(data['response']))
 
-user1 = User(61024852)
 
-user2 = User(70680070)
+user1 = User(70680070)
+
+user2 = User(61024852)
 
 user1 & user2
 
-# print(user1)
-# print(user2)
+print(user1)
+print(user2)
